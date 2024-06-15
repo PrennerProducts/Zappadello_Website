796ee330adc47747f638eda0f870290487b07819
PrennerProducts <prenner.products@gmail.com>
2024-06-15 22:30:58 +0200

    add grid to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,13 +15,13 @@ const Hero = () => {
         />
         <Spotlight className="top-28 -left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.03] bg-dot-black/[0.2] flex items-center justify-center absulute top-0 left-0 ">
-          {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        </div>
-        <div className="flex justify-center">
-          <div className="max-w-[89vw]"></div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.3] bg-dot-black/[0.2] flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-[89vw]">
           <h2 className="uppercase tracking-tightes">
             Dynamiic Web magic with Next
           </h2>
